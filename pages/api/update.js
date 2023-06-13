947473be3db3d6d162d112d280617e2a40c043cc
ayush <ayush.chauhan0008@gmail.com>
2023-06-13 17:24:16 +0530

    update todos

---
+++ pages/api/update.js
@@ -0,0 +1,18 @@
+import { table, getMinifiedRecord } from "./utils/airtable";
+
+export default async (req, res) => {
+    const { id, fields } = req.body
+
+    try {
+        const updatedRecords = await table.update([
+            { id, fields }
+        ])
+
+        res.statusCode = 200
+        res.json(getMinifiedRecord(updatedRecords[0]))
+    } catch (err) {
+        console.log(err)
+        res.statusCode = 418
+        res.json({ errorMessage: 'something went wrong' })
+    }
+}
