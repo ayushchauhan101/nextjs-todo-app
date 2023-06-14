607003515efbcb9eaa4f8bc3a0353a5e55aa667a
ayush <ayush.chauhan0008@gmail.com>
2023-06-14 23:02:11 +0530

    render todos

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from "next/head"
 import Navbar from "@/components/Navbar"
 
 import { table, minifyRecords } from "./api/utils/airtable"
+import Todo from "@/components/Todo"
 
-export default function index({ initialTodos }) {
+export default function index ({ initialTodos }) {
   // console.log(initialTodos)
   return (
     <div>
@@ -13,21 +14,26 @@ export default function index({ initialTodos }) {
       <Navbar />
       <main>
         <h1>Todos App</h1>
+        <ul>
+          { initialTodos.map(todo => <Todo key={ todo.id } todo={ todo } />) }
+        </ul>
       </main>
     </div>
   )
 }
 
 // fetch Airtable data on this component render or build
-export async function getServerSideProps(context) {
-  try {
+export async function getServerSideProps (context) {
+  try
+  {
     const todos = await table.select({}).firstPage()
     return {
       props: {
         initialTodos: minifyRecords(todos)
       }
     }
-  } catch (err) {
+  } catch (err)
+  {
     console.log(err)
     return {
       props: {
