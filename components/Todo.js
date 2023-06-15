4e529d16e4b71764d160c2e9cff0a1e166af9e4a
ayush <ayush.chauhan0008@gmail.com>
2023-06-15 22:54:42 +0530

    add updates to context

--- components/Todo.js
+++ components/Todo.js
@@ -1,11 +1,26 @@
+import { useContext } from "react"
+import { TodosContext } from "@/contexts/TodosContext"
+
 export default function Todo ({ todo }) {
+
+    const { updateTodos, deleteTodos } = useContext(TodosContext)
+
+    async function toggleCompletedHandler () {
+        const updatedFields = {
+            ...todo.fields,
+            completed: !todo.fields.completed
+        }
+        const updatedTodo = { id: todo.id, fields: updatedFields }
+        updateTodos(updatedTodo)
+    }
+
     return (
         <li className="bg-white flex items-center shadow-lg rounded-lg my-2 py-2 px-4 justify-between">
             <input
                 type="checkbox"
                 name="completed"
-                id="completed"
                 className="mr-2 h-5 w-5"
+                onChange={ toggleCompletedHandler }
                 checked={ todo.fields.completed }
             />
             <p className={ `flex-1 text-gray-800 ${todo.fields.completed && 'line-through'} ` } >
@@ -13,6 +28,7 @@ export default function Todo ({ todo }) {
             </p>
             <button
                 type="button"
+                onClick={ () => deleteTodos(todo.id) }
                 className="text-sm bg-red-400 hover:bg-red-600 hover:text-white py-1 px-2 rounded duration-100 ease-in">
                 Delete
             </button>
